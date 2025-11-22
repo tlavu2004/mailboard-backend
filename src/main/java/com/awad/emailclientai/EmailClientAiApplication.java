f02a2cdb3a1d876c0437af8153f20826ba783375
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2025-11-22 20:51:07 +0700

    chore(project): initialize Email Client AI project with Maven setup and basic application structure

---
+++ src/main/java/com/awad/emailclientai/EmailClientAiApplication.java
@@ -0,0 +1,13 @@
+package com.awad.emailclientai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailClientAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailClientAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awad/emailclientai/EmailClientAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awad.emailclientai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailClientAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
